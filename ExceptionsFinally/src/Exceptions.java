4f108fad7ff8189ad1018687c53e180cafc7b7d7
Kamila <kamilavrhcp@hotmail.com>
2022-05-20 13:13:41 -0300

    Exceptions Finally

---
+++ ExceptionsFinally/src/Exceptions.java
@@ -0,0 +1,17 @@
+
+public class Exceptions {
+
+  public static void main(String[] args) {
+    try {
+      int resultado = 25 / 0;
+      System.out.println(resultado);
+    } catch (ArithmeticException e) {
+      System.out.println("Erro tratado");
+      System.out.println(e);
+    } finally {
+      System.out.println("Bloco finally");
+    }
+
+  }
+
+}
