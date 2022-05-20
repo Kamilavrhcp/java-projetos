dad541a628ee364d7bc0a91b5f3ebadc60218eff
Kamila <kamilavrhcp@hotmail.com>
2022-05-20 12:30:32 -0300

    Fatoral
    Exercício fatoral

---
+++ acc-java-02-exercises-fatorial/src/main/java/com/trybe/acc/java/Fatorial.java
@@ -0,0 +1,18 @@
+package com.trybe.acc.java;
+
+public class Fatorial {
+
+  /**
+   * Fatorial.
+   */
+
+  public static void main(String[] args) {
+    // Seu código aqui
+    long fatorial18 = 6402373705728000L;
+
+    // Imprime a saída no console. OBS: descomente a próxima linha
+    System.out.println("O fatorial de 18 é igual a " + fatorial18);
+
+  }
+
+}
