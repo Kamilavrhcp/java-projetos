74876e485f4df0c6d9f60fd6146accf903a71000
Kamila <kamilavrhcp@hotmail.com>
2022-05-20 12:29:23 -0300

    Estado Lampada
    Exercícios estado lampada.

---
+++ acc-java-02-exercises-estado-lampada/src/main/java/com/trybe/acc/java/Lampada.java
@@ -0,0 +1,18 @@
+package com.trybe.acc.java;
+
+public class Lampada {
+
+  /**
+   * Ligar/ desligar lâmpada.
+   */
+
+  public static void main(String[] args) {
+    // Seu código aqui
+    boolean isLigada = true;
+
+    // Imprime a saída no console. OBS: descomente a próxima linha
+    System.out.println("A lampada ligada: " + isLigada + "!");
+
+  }
+
+}
