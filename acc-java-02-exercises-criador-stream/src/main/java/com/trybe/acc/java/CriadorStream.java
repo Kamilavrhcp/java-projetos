62a00f1eda2cd75e1b08f33e068f610cae1ba1a4
Kamila <kamilavrhcp@hotmail.com>
2022-05-20 13:27:43 -0300

    Usando Streams

---
+++ acc-java-02-exercises-criador-stream/src/main/java/com/trybe/acc/java/CriadorStream.java
@@ -0,0 +1,20 @@
+package com.trybe.acc.java;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CriadorStream {
+  public <T> Stream<T> criarDoItem(T item) {
+    return Stream.of(item);
+  }
+
+  public <T> Stream<T> criarDoArray(T[] array) {
+    return Arrays.stream(array);
+  }
+
+  public <T> Stream<T> criarDaLista(List<T> lista) {
+    return lista.stream();
+  }
+
+}
