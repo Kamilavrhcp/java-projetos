841dd0bcd7194d2842b2fcdef08330170dc667df
Kamila <kamilavrhcp@hotmail.com>
2022-05-26 02:21:13 -0300

    Revert "Revert "agrotis-api""

---
+++ agrotis-api/src/main/java/com/agrotis/crm/model/Laboratorio.java
@@ -0,0 +1,60 @@
+package com.agrotis.crm.model;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+public class Laboratorio {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@ManyToOne
+	private Cliente cliente;
+
+	public Laboratorio(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Laboratorio other = (Laboratorio) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
